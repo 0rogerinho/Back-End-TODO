379f28bc9adfc3787c84b9f7021c257e64728c44
0rogerinho <luaansilva11@gmail.com>
2023-09-01 23:46:57 -0300

    atualização da validação

--- src/validation/UserValidation.js
+++ src/validation/UserValidation.js
@@ -5,11 +5,10 @@ export const userValidation = async (email, password) => {
   const userId = await User.findOne({ email });
 
   console.log(userId.password);
-  if (!userId) throw new Error('user not found');
+  if (userId === null) throw new Error('user not found');
 
   const bcryptPass = compareSync(password, userId.password);
 
-  console.log(bcryptPass);
   if (!bcryptPass) throw new Error('invalid password');
 
   return { _id: userId['_id'] };
