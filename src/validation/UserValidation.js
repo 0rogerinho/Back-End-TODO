948dfe8502ac6eb75352ab79e07ba2656b6d56be
0rogerinho <luaansilva11@gmail.com>
2023-09-01 23:48:08 -0300

    remoção de console

--- src/validation/UserValidation.js
+++ src/validation/UserValidation.js
@@ -4,7 +4,6 @@ import { User } from '../models/UserModel.js';
 export const userValidation = async (email, password) => {
   const userId = await User.findOne({ email });
 
-  console.log(userId.password);
   if (userId === null) throw new Error('user not found');
 
   const bcryptPass = compareSync(password, userId.password);
