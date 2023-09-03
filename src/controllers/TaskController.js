96cabffcf10971ec4c45fee5d7dda78c6a26f850
devrogerinho <120338267+0rogerinho@users.noreply.github.com>
2023-09-03 15:22:56 -0300

    Update TaskController.js

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -6,10 +6,10 @@ export class TaskController {
   }
 
   async postTask(req, res) {
-    const { id, title } = req.body;
+    const { _id, title } = req.body;
 
     try {
-      await this.taskService.create({ user_id: id, title });
+      await this.taskService.create({ user_id: _id, title });
       return res.status(201).json({ message: 'Task created' });
     } catch (error) {
       return res.status(400).json(error.message);
